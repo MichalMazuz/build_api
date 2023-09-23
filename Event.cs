16d920e96158dd38ce620135e30bb859cf347a3d
“MichalMazuz” <michal7969@gmail.com>
2023-09-24 02:25:33 +0300

    initial commit

---
+++ Event.cs
@@ -0,0 +1,10 @@
+﻿namespace build_api
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime start { get; set; }
+        public DateTime end { get; set; }
+    }
+}
